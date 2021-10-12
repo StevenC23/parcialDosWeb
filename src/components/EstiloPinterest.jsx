5b456367dbc306ac2cc5b1a59c42f3423319a991
scarvajal0723 <scarvajal@vortexbird.com>
2021-10-12 13:26:07 -0500

    imagenes

--- src/components/EstiloPinterest.jsx
+++ src/components/EstiloPinterest.jsx
@@ -4,53 +4,53 @@ import Pin from './Pin.jsx'
 const EstiloPinterest = () => {
     return (
         <div style = {styles.pin_container}>
-            <Pin size='small'/>
-            <Pin size='small'/>
-            <Pin size='medium'/>
-            <Pin size='large'/>
-            <Pin size='medium'/>
-            <Pin size='small'/>
-            <Pin size='medium'/>
-            <Pin size='large'/>
-            <Pin size='small'/>
-            <Pin size='medium'/>
-            <Pin size='large'/>
-            <Pin size='small'/>
-            <Pin size='medium'/>
-            <Pin size='medium'/>
-            <Pin size='large'/>
-            <Pin size='medium'/>
-            <Pin size='large'/>
-            <Pin size='small'/>
-            <Pin size='medium'/>
-            <Pin size='large'/>
-            <Pin size='small'/>
-            <Pin size='medium'/>
-            <Pin size='medium'/>
-            <Pin size='medium'/>
-            <Pin size='large'/>
-            <Pin size='medium'/>
-            <Pin size='small'/>
-            <Pin size='medium'/>
-            <Pin size='medium'/>
-            <Pin size='large'/>
-            <Pin size='medium'/>
-            <Pin size='small'/>
-            <Pin size='medium'/>
-            <Pin size='large'/>
-            <Pin size='small'/>
-            <Pin size='medium'/>
-            <Pin size='large'/>
-            <Pin size='medium'/>
-            <Pin size='large'/>
-            <Pin size='medium'/>
-            <Pin size='small'/>
-            <Pin size='medium'/>
-            <Pin size='large'/>
-            <Pin size='small'/>
-            <Pin size='medium'/>
-            <Pin size='large'/>
-            <Pin size='small'/>
+            <Pin size='small' img='https://picsum.photos/300/450'/>
+            <Pin size='small' img='https://picsum.photos/200/150'/>
+            <Pin size='medium' img='https://picsum.photos/700/350'/>
+            <Pin size='large' img='https://picsum.photos/300/450'/>
+            <Pin size='medium' img='https://picsum.photos/900/850'/>
+            <Pin size='small' img='https://picsum.photos/600/350'/>
+            <Pin size='medium' img='https://picsum.photos/300/250'/>
+            <Pin size='large' img='https://picsum.photos/500/250'/>
+            <Pin size='small' img='https://picsum.photos/400/150'/>
+            <Pin size='medium' img='https://picsum.photos/700/750'/>
+            <Pin size='large' img='https://picsum.photos/800/650'/>
+            <Pin size='small' img='https://picsum.photos/500/850'/>
+            <Pin size='medium' img='https://picsum.photos/600/650'/>
+            <Pin size='medium' img='https://picsum.photos/400/350'/>
+            <Pin size='large' img='https://picsum.photos/100/250'/>
+            <Pin size='medium' img='https://picsum.photos/300/650'/>
+            <Pin size='large' img='https://picsum.photos/900/950'/>
+            <Pin size='small' img='https://picsum.photos/600/850'/>
+            <Pin size='medium' img='https://picsum.photos/400/750'/>
+            <Pin size='large' img='https://picsum.photos/300/150'/>
+            <Pin size='small' img='https://picsum.photos/700/350'/>
+            <Pin size='medium'img='https://picsum.photos/800/250'/>
+            <Pin size='medium' img='https://picsum.photos/900/150'/>
+            <Pin size='medium' img='https://picsum.photos/400/650'/>
+            <Pin size='large' img='https://picsum.photos/500/450'/>
+            <Pin size='medium' img='https://picsum.photos/600/550'/>
+            <Pin size='small' img='https://picsum.photos/100/450'/>
+            <Pin size='medium' img='https://picsum.photos/200/750'/>
+            <Pin size='medium' img='https://picsum.photos/300/850'/>
+            <Pin size='large' img='https://picsum.photos/400/950'/>
+            <Pin size='medium' img='https://picsum.photos/500/650'/>
+            <Pin size='small' img='https://picsum.photos/600/350'/>
+            <Pin size='medium' img='https://picsum.photos/900/150'/>
+            <Pin size='large' img='https://picsum.photos/800/250'/>
+            <Pin size='small' img='https://picsum.photos/700/650'/>
+            <Pin size='medium' img='https://picsum.photos/400/950'/>
+            <Pin size='large' img='https://picsum.photos/500/750'/>
+            <Pin size='medium' img='https://picsum.photos/100/250'/>
+            <Pin size='large' img='https://picsum.photos/200/150'/>
+            <Pin size='medium' img='https://picsum.photos/700/450'/>
+            <Pin size='small' img='https://picsum.photos/300/550'/>
+            <Pin size='medium' img='https://picsum.photos/400/450'/>
+            <Pin size='large' img='https://picsum.photos/600/950'/>
+            <Pin size='small' img='https://picsum.photos/400/750'/>
+            <Pin size='medium' img='https://picsum.photos/600/650'/>
+            <Pin size='large' img='https://picsum.photos/700/450'/>
+            <Pin size='small' img='https://picsum.photos/300/250'/>
 
         </div>
     );
--- src/components/HeaderSmall.jsx
+++ src/components/HeaderSmall.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 const HeaderSmall = () => {
     return (
         <div style = {styles.headerSmall}>
-            
+            <div style = {styles.scrollmenu}>
+                <a style = {styles.link} href="#parati"><strong>Para ti</strong></a>
+                <a style = {styles.link} href="#nuevo"><strong>Nuevo</strong></a>
+                <a style = {styles.link} href="#hoy"><strong>Hoy</strong></a>
+                <a style = {styles.link} href="#masvistos"><strong>Mas vistos</strong></a>
+                <a style = {styles.link} href="#masgustados"><strong>Mas gustados</strong></a>
+                <a style = {styles.link} href="#famosos"><strong>Famosos</strong></a>
+                <a style = {styles.link} href="#famosos"><strong>Entrevistas</strong></a>
+                <a style = {styles.link} href="#famosos"><strong>Tendencias</strong></a>
+                <a style = {styles.link} href="#famosos"><strong>Politica</strong></a>
+                <a style = {styles.link} href="#famosos"><strong>Deportes</strong></a>
+            </div>
         </div>
     );
 };
@@ -13,7 +24,28 @@ const styles = {
         display: 'flex',
         width: '100vw',
         height: '80px',
-        backgroundColor: 'blue',
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    // scroll
+    scrollmenu: {
+        padding: '5px',
+        backgroundColor: 'white',
+        overflow: 'auto',
+        whiteSpace: 'nowrap',
+        display: 'flex',
+        // justifyContent: 'center',
+        alignItems: 'center',
+    },
+      
+    link: {
+        display: 'inline-block',
+        color: 'black',
+        textAlign: 'center',
+        padding: '10px',
+        textDecoration: 'none',
     }
 }
 export default HeaderSmall;
